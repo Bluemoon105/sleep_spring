5c5f278f8a494c9f20698d01ec14cffea9c4d93b
unknown <lyj69641@gmail.com>
2025-11-06 23:10:56 +0900

    fix version

--- src/main/java/com/example/sleep/SleepApplication.java
+++ src/main/java/com/example/sleep/SleepApplication.java
@@ -1,9 +1,11 @@
 package com.example.sleep;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.sleep.mapper") // ✅ 추가
 public class SleepApplication {
     public static void main(String[] args) {
         SpringApplication.run(SleepApplication.class, args);

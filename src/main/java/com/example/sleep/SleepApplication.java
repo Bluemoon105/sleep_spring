c3129546929c0226deb03515ba8f6fcd184ba7d0
Bluemoon105 <lyj69641@gmail.com>
2025-11-07 10:24:58 +0900

    edit ver

--- src/main/java/com/example/sleep/SleepApplication.java
+++ src/main/java/com/example/sleep/SleepApplication.java
@@ -4,8 +4,8 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.example.sleep.mapper")
 @SpringBootApplication
-@MapperScan("com.example.sleep.mapper") // ✅ 추가
 public class SleepApplication {
     public static void main(String[] args) {
         SpringApplication.run(SleepApplication.class, args);
